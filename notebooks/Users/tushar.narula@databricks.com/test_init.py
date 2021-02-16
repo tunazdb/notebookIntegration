cf9ac3568964089915cf870b5d20aee79d017c47
tunazdb <78734714+tunazdb@users.noreply.github.com>
2021-02-16 12:17:22 +0530

    First Commit

---
+++ notebooks/Users/tushar.narula@databricks.com/test_init.py
@@ -0,0 +1,17 @@
+# Databricks notebook source
+ls -ltr /databricks/init_scripts/
+
+# COMMAND ----------
+
+ls /dbfs/databricks/init
+
+# COMMAND ----------
+
+mkdir /dbfs/logs_location
+
+# COMMAND ----------
+
+ls /dbfs/
+
+# COMMAND ----------
+
